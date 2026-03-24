2e773e69eaa9d08e3f8e578895062f58c0141dc6
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-03-24 12:46:21 +0530

    image fixed

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-             src="./src/assets/main.png.jpg"
+             src="/main.png.jpg"
             alt="Card"
             height={500}
           />
